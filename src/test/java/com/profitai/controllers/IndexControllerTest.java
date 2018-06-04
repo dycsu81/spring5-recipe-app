047ef7b680de8cc3ee022df0631ef9f666dccabc
David Su <dycsu81@gmail.com>
2018-06-05 01:04:51 +0800

    Use Mockito and unit test indexController

---
+++ src/test/java/com/profitai/controllers/IndexControllerTest.java
@@ -0,0 +1,39 @@
+package com.profitai.controllers;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import com.profitai.service.RecipeService;
+
+
+public class IndexControllerTest {
+
+	@Mock
+	RecipeService recipeService;
+	
+	@Mock
+	Model model;
+	
+	IndexController controller;
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		controller = new IndexController(recipeService);
+	}
+
+	@Test
+	public void testGetIndexPage() {
+		String viewName = controller.getIndexPage(model);
+		assertEquals("index", viewName);
+		verify(recipeService, times(1)).getRecipes();
+	}
+
+}
