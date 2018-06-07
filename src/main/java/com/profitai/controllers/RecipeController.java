f49bb4205738ce4f4f94bf267a09bfd0a9e8f603
David Su <dycsu81@gmail.com>
2018-06-07 17:12:13 +0800

    Add validators to form fields

--- src/main/java/com/profitai/controllers/RecipeController.java
+++ src/main/java/com/profitai/controllers/RecipeController.java
@@ -1,14 +1,16 @@
 package com.profitai.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -61,8 +63,18 @@ public class RecipeController {
 	}
 	
 	@PostMapping("recipe")
-	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+	public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
 		log.debug("saveOrUpdate");
+		
+		if (bindingResult.hasErrors()) {
+			bindingResult.getAllErrors().forEach(objErr -> {
+				log.error(objErr.toString());
+			});
+			
+			return "recipe/recipeform";
+		}
+		
+		
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 		
 		return "redirect:/recipe/" + savedCommand.getId() + "/show";
